2066b603d7dcde1ba00596aba4ec4d44613615e1
Amadeus1791 <sezuan01@gmail.com>
2025-01-19 18:41:22 +0100

    Modernize Gate Figures (AND, OR, XOR)
    Signed-off-by: Amadeus1791 <sezuan01@gmail.com>

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/figures/LogicColorConstants.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/figures/LogicColorConstants.java
@@ -16,9 +16,10 @@ import org.eclipse.swt.graphics.Color;
 
 public interface LogicColorConstants {
 
-	public final static Color andGate = new Color(null, 220, 70, 80);
-	public final static Color orGate = new Color(null, 0, 134, 255);
-	public final static Color xorGate = new Color(null, 240, 240, 40);
+	public final static Color andGate = new Color(null, 204, 61, 61);
+	public final static Color orGate = new Color(null, 41, 121, 255);
+	public final static Color xorGate = new Color(null, 255, 204, 0);
+	public static final Color outlineColor = new Color(null, 64, 64, 64);
 	public final static Color logicGreen = new Color(null, 123, 174, 148);
 	public final static Color logicHighlight = new Color(null, 66, 166, 115);
 	public final static Color connectorGreen = new Color(null, 0, 69, 40);
