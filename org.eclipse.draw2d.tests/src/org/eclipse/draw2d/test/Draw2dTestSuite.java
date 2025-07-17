b2f45e91777015540047010aa086736395e65f7a
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-07-17 22:30:39 +0200

    Migrate Label test case from WindowBuilder project to Draw2D
    This test case is used by the WindowBuilder project to verify the
    correctness of its forked Label class. Because this class is about to be
    deleted in favor of the Draw2D class, the corresponding test case should
    be moved to the GEF project.

--- org.eclipse.draw2d.tests/src/org/eclipse/draw2d/test/Draw2dTestSuite.java
+++ org.eclipse.draw2d.tests/src/org/eclipse/draw2d/test/Draw2dTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2024 IBM Corporation and others.
+ * Copyright (c) 2000, 2025 IBM Corporation and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -59,7 +59,8 @@ import org.junit.platform.suite.api.Suite;
 	ShapeTest.class,
 	InsetsTest.class,
 	DirectedGraphLayoutTest.class,
-	ScrollPaneTests.class
+	ScrollPaneTests.class,
+	LabelTest.class
 })
 public class Draw2dTestSuite {
 }
