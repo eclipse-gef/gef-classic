15ffe2ce3ac451cc71ca33734cf01e81091ef4a4
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-09-06 16:37:53 +0200

    Enable "deprecation" compiler warnings

--- org.eclipse.draw2d.tests/src/org/eclipse/draw2d/test/ScaledGraphicsTest.java
+++ org.eclipse.draw2d.tests/src/org/eclipse/draw2d/test/ScaledGraphicsTest.java
@@ -38,7 +38,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-@SuppressWarnings("removal")
+@SuppressWarnings({ "removal", "deprecation" })
 public class ScaledGraphicsTest {
 
 	static Stream<Arguments> singleValueTestCombinations() {
