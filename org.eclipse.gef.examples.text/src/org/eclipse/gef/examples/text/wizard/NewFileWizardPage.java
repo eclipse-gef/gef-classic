1e6dd525a0e665d4fa37b061f9decedc06a82c4f
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-09-20 21:20:13 +0200

    Use WizardNewFileCreationPage for "New Document Example" wizard page
    The Eclipse IDE already provides a perfectly fine wizard page for
    creating new files. There is no need to re-implement existing
    functionality.

--- org.eclipse.gef.examples.text/src/org/eclipse/gef/examples/text/wizard/NewFileWizardPage.java
+++ org.eclipse.gef.examples.text/src/org/eclipse/gef/examples/text/wizard/NewFileWizardPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2023 IBM Corporation and others.
+ * Copyright (c) 2005, 2025 IBM Corporation and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -12,50 +12,30 @@
  *******************************************************************************/
 package org.eclipse.gef.examples.text.wizard;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.dialogs.IDialogPage;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.ui.dialogs.ContainerSelectionDialog;
+import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
 
 /**
  * The "New" wizard page allows setting the container for the new file as well
  * as the file name. The page will only accept file name without the extension
  * OR with the extension that matches the expected one (text).
  */
-public class NewFileWizardPage extends WizardPage {
-	private Text containerText;
-
-	private Text fileText;
-
-	private final ISelection selection;
+public class NewFileWizardPage extends WizardNewFileCreationPage {
 
 	/**
 	 * Constructor for SampleNewWizardPage.
 	 *
 	 */
-	public NewFileWizardPage(ISelection selection) {
-		super("wizardPage"); //$NON-NLS-1$
+	public NewFileWizardPage(IStructuredSelection selection) {
+		super("wizardPage", selection); //$NON-NLS-1$
 		setTitle("GEF WYSIWYG Text Document"); //$NON-NLS-1$
 		setDescription("""
 				This wizard creates a GEF-based WYSIWYG text document with \
 				a *.text. extension.  Choose a container and file name for the new\
 				 resource."""); //$NON-NLS-1$
-		this.selection = selection;
 	}
 
 	/**
@@ -63,124 +43,8 @@ public class NewFileWizardPage extends WizardPage {
 	 */
 	@Override
 	public void createControl(Composite parent) {
-		Composite container = new Composite(parent, SWT.NULL);
-		GridLayout layout = new GridLayout();
-		container.setLayout(layout);
-		layout.numColumns = 3;
-		layout.verticalSpacing = 9;
-		Label label = new Label(container, SWT.NULL);
-		label.setText("&Container:"); //$NON-NLS-1$
-
-		containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
-		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-		containerText.setLayoutData(gd);
-		containerText.addModifyListener(e -> dialogChanged());
-
-		Button button = new Button(container, SWT.PUSH);
-		button.setText("Browse..."); //$NON-NLS-1$
-		button.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				handleBrowse();
-			}
-		});
-		label = new Label(container, SWT.NULL);
-		label.setText("&File name:"); //$NON-NLS-1$
-
-		fileText = new Text(container, SWT.BORDER | SWT.SINGLE);
-		gd = new GridData(GridData.FILL_HORIZONTAL);
-		fileText.setLayoutData(gd);
-		fileText.addModifyListener(e -> dialogChanged());
-		initialize();
-		dialogChanged();
-		setControl(container);
-	}
-
-	/**
-	 * Tests if the current workbench selection is a suitable container to use.
-	 */
-	private void initialize() {
-		if (selection != null && selection.isEmpty() == false && selection instanceof IStructuredSelection ssel) {
-			if (ssel.size() > 1) {
-				return;
-			}
-			Object obj = ssel.getFirstElement();
-			if (obj instanceof IResource res) {
-				IContainer container;
-				if (obj instanceof IContainer cont) {
-					container = cont;
-				} else {
-					container = res.getParent();
-				}
-				containerText.setText(container.getFullPath().toString());
-			}
-		}
-		fileText.setText("new_file.text"); //$NON-NLS-1$
-	}
-
-	/**
-	 * Uses the standard container selection dialog to choose the new value for the
-	 * container field.
-	 */
-	private void handleBrowse() {
-		ContainerSelectionDialog dialog = new ContainerSelectionDialog(getShell(),
-				ResourcesPlugin.getWorkspace().getRoot(), false, "Select new file container"); //$NON-NLS-1$
-		if (dialog.open() == ContainerSelectionDialog.OK) {
-			Object[] result = dialog.getResult();
-			if (result.length == 1) {
-				containerText.setText(((Path) result[0]).toString());
-			}
-		}
-	}
-
-	/**
-	 * Ensures that both text fields are set.
-	 */
-	private void dialogChanged() {
-		IResource container = ResourcesPlugin.getWorkspace().getRoot().findMember(new Path(getContainerName()));
-		String fileName = getFileName();
-
-		if (getContainerName().length() == 0) {
-			updateStatus("File container must be specified"); //$NON-NLS-1$
-			return;
-		}
-		if (container == null || (container.getType() & (IResource.PROJECT | IResource.FOLDER)) == 0) {
-			updateStatus("File container must exist"); //$NON-NLS-1$
-			return;
-		}
-		if (!container.isAccessible()) {
-			updateStatus("Project must be writable"); //$NON-NLS-1$
-			return;
-		}
-		if (fileName.length() == 0) {
-			updateStatus("File name must be specified"); //$NON-NLS-1$
-			return;
-		}
-		if (fileName.replace('\\', '/').indexOf('/', 1) > 0) {
-			updateStatus("File name must be valid"); //$NON-NLS-1$
-			return;
-		}
-		int dotLoc = fileName.lastIndexOf('.');
-		if (dotLoc != -1) {
-			String ext = fileName.substring(dotLoc + 1);
-			if (ext.equalsIgnoreCase("text") == false) { //$NON-NLS-1$
-				updateStatus("File extension must be \"text\""); //$NON-NLS-1$
-				return;
-			}
-		}
-		updateStatus(null);
-	}
-
-	private void updateStatus(String message) {
-		setErrorMessage(message);
-		setPageComplete(message == null);
-	}
-
-	public String getContainerName() {
-		return containerText.getText();
-	}
-
-	public String getFileName() {
-		return fileText.getText();
+		super.createControl(parent);
+		setFileName("new_file.text"); //$NON-NLS-1$
+		setPageComplete(validatePage());
 	}
 }
