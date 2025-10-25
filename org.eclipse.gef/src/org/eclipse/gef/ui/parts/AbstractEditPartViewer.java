eclipse-gef/gef-classic

commit 1bd28834343bce2bf73cbf66d9cb2accc54382f0

Add autoscale disabled mode to allow drawing on unscaled GC

org.eclipse.gef/src/org/eclipse/gef/ui/parts/AbstractEditPartViewer.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DragSource;
 import org.eclipse.swt.dnd.DropTarget;
@@ -46,6 +47,7 @@ import org.eclipse.jface.viewers.SelectionChangedEvent;
 
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.internal.InternalDraw2dUtils;
 
 import org.eclipse.gef.AccessibleEditPart;
 import org.eclipse.gef.EditDomain;
@@ -55,6 +57,7 @@ import org.eclipse.gef.EditPartViewer;
 import org.eclipse.gef.KeyHandler;
 import org.eclipse.gef.RootEditPart;
 import org.eclipse.gef.SelectionManager;
+import org.eclipse.gef.internal.InternalGEFPlugin;
 
 /**
  * The base implementation for EditPartViewer.
@@ -484,6 +487,11 @@ public abstract class AbstractEditPartViewer implements EditPartViewer {
 		if (contextMenu != null) {
 			control.setMenu(contextMenu.createContextMenu(getControl()));
 		}
+		if (InternalDraw2dUtils.disableAutoscale) {
+			control.addListener(SWT.ZoomChanged,
+					e -> setProperty(InternalGEFPlugin.MONITOR_SCALE_PROPERTY, e.detail / 100.0));
+		}
+		setProperty(InternalGEFPlugin.MONITOR_SCALE_PROPERTY, calculateScale());
 	}
 
 	/**
@@ -855,4 +863,10 @@ public abstract class AbstractEditPartViewer implements EditPartViewer {
 	public void unregisterAccessibleEditPart(AccessibleEditPart acc) {
 	}
 
+	private double calculateScale() {
+		if (!InternalDraw2dUtils.disableAutoscale || control == null) {
+			return 1.0;
+		}
+		return control.getMonitor().getZoom() / 100.0;
+	}
 }

org.eclipse.draw2d/src/org/eclipse/draw2d/FigureUtilities.java
@@ -23,6 +23,7 @@ import org.eclipse.swt.widgets.Shell;
 
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.draw2d.internal.InternalDraw2dUtils;
 
 /**
  * Provides miscellaneous Figure operations.
@@ -73,7 +74,9 @@ public class FigureUtilities {
 	@Deprecated
 	protected static GC getGC() {
 		if (gc == null) {
-			gc = new GC(new Shell());
+			Shell shell = new Shell();
+			InternalDraw2dUtils.configureForAutoscalingMode(shell);
+			gc = new GC(shell);
 			appliedFont = gc.getFont();
 		}
 		return gc;
@@ -424,5 +427,4 @@ public class FigureUtilities {
 		}
 		return !figBounds.isEmpty();
 	}
-
 }

org.eclipse.draw2d/src/org/eclipse/draw2d/internal/InternalDraw2dUtils.java
@@ -1,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Yatta and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Yatta - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.draw2d.internal;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Control;
+
+public class InternalDraw2dUtils {
+	/**
+	 * System property that controls the disablement of any autoScale functionality.
+	 * Currently it only has effects when executed on Windows.
+	 *
+	 * <ul>
+	 * <li><b>false</b>: autoScale functionality is enabled</li>
+	 * <li><b>true</b>: autoScale functionality is disabled<</li>
+	 *
+	 * </ul>
+	 * The current default is "false".
+	 */
+	private static final String DRAW2D_DISABLE_AUTOSCALE = "draw2d.disableAutoscale"; //$NON-NLS-1$
+
+	public static boolean disableAutoscale;
+
+	static {
+		disableAutoscale = "win32".equals(SWT.getPlatform()) //$NON-NLS-1$
+				&& Boolean.parseBoolean(System.getProperty(DRAW2D_DISABLE_AUTOSCALE));
+	}
+
+	public static void configureForAutoscalingMode(Control control) {
+		if (control != null && disableAutoscale) {
+			control.setData("AUTOSCALE_DISABLED", true); //$NON-NLS-1$
+		}
+	}
+}

org.eclipse.gef/src/org/eclipse/gef/editparts/FreeformGraphicalRootEditPart.java
@@ -20,6 +20,7 @@ import org.eclipse.draw2d.FreeformLayeredPane;
 import org.eclipse.draw2d.FreeformViewport;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.LayeredPane;
+import org.eclipse.draw2d.ScalableFreeformLayeredPane;
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
 
@@ -30,6 +31,7 @@ import org.eclipse.gef.EditPartViewer;
 import org.eclipse.gef.LayerConstants;
 import org.eclipse.gef.Request;
 import org.eclipse.gef.SnapToGrid;
+import org.eclipse.gef.internal.InternalGEFPlugin;
 import org.eclipse.gef.tools.MarqueeDragTracker;
 
 /**
@@ -86,7 +88,7 @@ import org.eclipse.gef.tools.MarqueeDragTracker;
  */
 public class FreeformGraphicalRootEditPart extends SimpleRootEditPart implements LayerConstants, LayerManager {
 
-	private LayeredPane innerLayers;
+	private ScalableFreeformLayeredPane innerLayers;
 	private LayeredPane printableLayers;
 	private final PropertyChangeListener gridListener = evt -> {
 		String property = evt.getPropertyName();
@@ -102,7 +104,8 @@ public class FreeformGraphicalRootEditPart extends SimpleRootEditPart implements
 	@Override
 	protected IFigure createFigure() {
 		FreeformViewport viewport = new FreeformViewport();
-		innerLayers = new FreeformLayeredPane();
+		innerLayers = new ScalableFreeformLayeredPane();
+		this.addEditPartListener(InternalGEFPlugin.createAutoscaleEditPartListener(innerLayers));
 		createLayers(innerLayers);
 		viewport.setContents(innerLayers);
 		return viewport;

org.eclipse.gef/src/org/eclipse/gef/editparts/ScalableRootEditPart.java
@@ -32,6 +32,7 @@ import org.eclipse.gef.DragTracker;
 import org.eclipse.gef.LayerConstants;
 import org.eclipse.gef.Request;
 import org.eclipse.gef.SnapToGrid;
+import org.eclipse.gef.internal.InternalGEFPlugin;
 import org.eclipse.gef.tools.MarqueeDragTracker;
 
 /**
@@ -121,7 +122,7 @@ public class ScalableRootEditPart extends SimpleRootEditPart implements LayerCon
 
 	}
 
-	private LayeredPane innerLayers;
+	private ScalableLayeredPane innerLayers;
 	private LayeredPane printableLayers;
 	private ScalableLayeredPane scaledLayers;
 	private final PropertyChangeListener gridListener = (PropertyChangeEvent evt) -> {
@@ -174,7 +175,8 @@ public class ScalableRootEditPart extends SimpleRootEditPart implements LayerCon
 	protected IFigure createFigure() {
 		Viewport viewport = createViewport();
 
-		innerLayers = new LayeredPane();
+		innerLayers = new ScalableLayeredPane();
+		this.addEditPartListener(InternalGEFPlugin.createAutoscaleEditPartListener(innerLayers));
 		createLayers(innerLayers);
 
 		viewport.setContents(innerLayers);

org.eclipse.gef/src/org/eclipse/gef/internal/InternalGEFPlugin.java
@@ -13,6 +13,7 @@
 
 package org.eclipse.gef.internal;
 
+import java.beans.PropertyChangeListener;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
@@ -30,12 +31,17 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 import org.eclipse.draw2d.BasicColorProvider;
 import org.eclipse.draw2d.ColorProvider;
+import org.eclipse.draw2d.ScalableFigure;
 
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.EditPartListener;
 import org.eclipse.gef.GEFColorProvider;
 
 import org.osgi.framework.BundleContext;
 
 public class InternalGEFPlugin extends AbstractUIPlugin {
+	/** Monitor scale property */
+	public static final String MONITOR_SCALE_PROPERTY = "monitorScale"; //$NON-NLS-1$
 
 	private static BundleContext context;
 	private static AbstractUIPlugin singleton;
@@ -126,5 +132,32 @@ public class InternalGEFPlugin extends AbstractUIPlugin {
 		} catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
 			throw new RuntimeException("Failed to instantiate Cursor", e); //$NON-NLS-1$
 		}
+	}
+
+	public static EditPartListener createAutoscaleEditPartListener(ScalableFigure figure) {
+		final PropertyChangeListener autoScaleListener = evt -> {
+			if (InternalGEFPlugin.MONITOR_SCALE_PROPERTY.equals(evt.getPropertyName()) && evt.getNewValue() != null) {
+				double newValue = (double) evt.getNewValue();
+				figure.setScale(newValue);
+			}
+		};
+
+		return new EditPartListener.Stub() {
+			@Override
+			public void partActivated(EditPart editpart) {
+				editpart.getViewer().addPropertyChangeListener(autoScaleListener);
+				try {
+					double scale = (double) editpart.getViewer().getProperty(InternalGEFPlugin.MONITOR_SCALE_PROPERTY);
+					figure.setScale(scale);
+				} catch (NullPointerException | ClassCastException e) {
+					// no value available
+				}
+			}
+
+			@Override
+			public void partDeactivated(EditPart editpart) {
+				editpart.getViewer().removePropertyChangeListener(autoScaleListener);
+			}
+		};
 	}
 }

org.eclipse.gef/src/org/eclipse/gef/internal/SimpleAutoscaledRootEditPart.java
@@ -1,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Yatta and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Yatta - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gef.internal;
+
+import org.eclipse.draw2d.ScalableFigure;
+import org.eclipse.draw2d.ScalableLayeredPane;
+
+import org.eclipse.gef.editparts.SimpleRootEditPart;
+
+public final class SimpleAutoscaledRootEditPart extends SimpleRootEditPart {
+
+	@Override
+	protected final ScalableFigure createFigure() {
+		ScalableFigure scalableFigure = new ScalableLayeredPane();
+		this.addEditPartListener(InternalGEFPlugin.createAutoscaleEditPartListener(scalableFigure));
+		return scalableFigure;
+	}
+
+	@Override
+	public ScalableFigure getFigure() {
+		return (ScalableFigure) super.getFigure();
+	}
+}

org.eclipse.gef/src/org/eclipse/gef/ui/palette/PaletteViewer.java
@@ -31,7 +31,7 @@ import org.eclipse.draw2d.IFigure;
 import org.eclipse.gef.EditDomain;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.GraphicalEditPart;
-import org.eclipse.gef.editparts.SimpleRootEditPart;
+import org.eclipse.gef.internal.SimpleAutoscaledRootEditPart;
 import org.eclipse.gef.internal.ui.palette.PaletteSelectionTool;
 import org.eclipse.gef.internal.ui.palette.editparts.DrawerEditPart;
 import org.eclipse.gef.internal.ui.palette.editparts.PaletteStackEditPart;
@@ -127,7 +127,7 @@ public class PaletteViewer extends ScrollingGraphicalViewer {
 	 */
 	@Override
 	protected void createDefaultRoot() {
-		setRootEditPart(new SimpleRootEditPart());
+		setRootEditPart(new SimpleAutoscaledRootEditPart());
 	}
 
 	private void disposeFont() {

commit 2cb5e738bd1eae7cabbff664d3f7e42e3b74b4f8

Move configuration of auto-scaling mode to GraphicalViewerImpl
The SWT-based auto-scaling must only be disabled for viewers that are
based on IFigure's and can't therefore be done in the
AbstractEditPartViewer.

Otherwise the Outline view, which is populated with a TreeViewer, is
always drawn at 100% zoom, as there is no ScalableFigure to scale its
content to the monitor zoom.

Regression from 1bd28834343bce2bf73cbf66d9cb2accc54382f0

org.eclipse.gef/src/org/eclipse/gef/ui/parts/AbstractEditPartViewer.java
@@ -46,7 +46,6 @@ import org.eclipse.jface.viewers.SelectionChangedEvent;
 
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.geometry.Point;
-import org.eclipse.draw2d.internal.InternalDraw2dUtils;
 
 import org.eclipse.gef.AccessibleEditPart;
 import org.eclipse.gef.EditDomain;
@@ -56,7 +55,6 @@ import org.eclipse.gef.EditPartViewer;
 import org.eclipse.gef.KeyHandler;
 import org.eclipse.gef.RootEditPart;
 import org.eclipse.gef.SelectionManager;
-import org.eclipse.gef.internal.InternalGEFPlugin;
 
 /**
  * The base implementation for EditPartViewer.
@@ -485,10 +483,6 @@ public abstract class AbstractEditPartViewer implements EditPartViewer {
 		refreshDropTargetAdapter();
 		if (contextMenu != null) {
 			control.setMenu(contextMenu.createContextMenu(getControl()));
-		}
-		if (InternalDraw2dUtils.isAutoScaleEnabled()) {
-			InternalDraw2dUtils.configureForAutoscalingMode(control,
-					scale -> setProperty(InternalGEFPlugin.MONITOR_SCALE_PROPERTY, scale));
 		}
 	}
 

org.eclipse.gef/src/org/eclipse/gef/ui/parts/GraphicalViewerImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2025 IBM Corporation and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -38,6 +38,7 @@ import org.eclipse.draw2d.ExclusionSearch;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.LightweightSystem;
 import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.internal.InternalDraw2dUtils;
 
 import org.eclipse.gef.AccessibleEditPart;
 import org.eclipse.gef.EditDomain;
@@ -53,6 +54,7 @@ import org.eclipse.gef.MouseWheelHelper;
 import org.eclipse.gef.RootEditPart;
 import org.eclipse.gef.editparts.LayerManager;
 import org.eclipse.gef.editparts.ScalableRootEditPart;
+import org.eclipse.gef.internal.InternalGEFPlugin;
 
 /**
  * An EditPartViewer implementation based on {@link org.eclipse.draw2d.IFigure
@@ -291,6 +293,10 @@ public class GraphicalViewerImpl extends AbstractEditPartViewer implements Graph
 				handleFocusLost(e);
 			}
 		});
+		if (InternalDraw2dUtils.isAutoScaleEnabled()) {
+			InternalDraw2dUtils.configureForAutoscalingMode(getControl(),
+					scale -> setProperty(InternalGEFPlugin.MONITOR_SCALE_PROPERTY, scale));
+		}
 	}
 
 	/**