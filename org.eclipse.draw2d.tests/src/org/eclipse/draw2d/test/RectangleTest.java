eclipse-gef/gef-classic

commit 5f3da64668ae1fe9f977fc95a9874f16ffd0fe04

changed scale to scale right edge, not width

org.eclipse.draw2d/src/org/eclipse/draw2d/geometry/Rectangle.java
@@ -630,10 +630,12 @@ public final Rectangle scale(double scaleFactor) {
  * @since 2.0
  */
 public Rectangle scale(double scaleX, double scaleY) {
+	int oldX = x;
+	int oldY = y;
 	x = (int)(Math.floor(x * scaleX));
 	y = (int)(Math.floor(y * scaleY));
-	width = (int)(Math.floor(width * scaleX));
-	height = (int)(Math.floor(height * scaleY));
+	width = (int)(Math.floor((oldX + width) * scaleX)) - x;
+	height = (int)(Math.floor((oldY + height) * scaleY)) - y;
 	return this;
 }
 

commit 0c6b0ee6ba9d9615eaa31da48ea44ef7920010f6

Harden scaling algorithm in Rectangle/SWTGraphics #829
This change avoids the rounding errors that were introduced with
5f3da64668ae1fe9f977fc95a9874f16ffd0fe04. By combining a rounded-down
value with a rounded-up value, the resulting width and height might be
one point larger than it should be, leading to off-by-one errors.

org.eclipse.draw2d/src/org/eclipse/draw2d/SWTGraphics.java
@@ -116,10 +116,10 @@ public class SWTGraphics extends Graphics {
 
 		@Override
 		public Rectangle getBoundingBox(Rectangle rect) {
-			rect.x = (int) left;
-			rect.y = (int) top;
-			rect.width = (int) Math.ceil(right) - rect.x;
-			rect.height = (int) Math.ceil(bottom) - rect.y;
+			rect.x = (int) Math.floor(left);
+			rect.y = (int) Math.floor(top);
+			rect.width = (int) Math.ceil(right - left);
+			rect.height = (int) Math.ceil(bottom - top);
 			return rect;
 		}
 

org.eclipse.draw2d/src/org/eclipse/draw2d/geometry/Rectangle.java
@@ -900,12 +900,10 @@ public class Rectangle implements Cloneable, java.io.Serializable, Translatable 
 	 * @since 2.0
 	 */
 	public Rectangle scale(double scaleX, double scaleY) {
-		int oldX = x;
-		int oldY = y;
-		x = (int) (Math.floor(x * scaleX));
-		y = (int) (Math.floor(y * scaleY));
-		width = (int) (Math.ceil((oldX + width) * scaleX)) - x;
-		height = (int) (Math.ceil((oldY + height) * scaleY)) - y;
+		x = (int) Math.floor(x * scaleX);
+		y = (int) Math.floor(y * scaleY);
+		width = (int) Math.ceil(width * scaleX);
+		height = (int) Math.ceil(height * scaleY);
 		return this;
 	}
 

org.eclipse.draw2d.tests/src/org/eclipse/draw2d/test/RectangleTest.java
@@ -468,6 +468,11 @@ public class RectangleTest extends BaseTestCase {
 		template = new Rectangle(10, 10, 20, 20);
 		assertSame(template, template.scale(0.5, 0.5));
 		assertEquals(5, 5, 10, 10, template);
+
+		// check work scale(double) with rounding errors
+		template = new Rectangle(10, 10, 52, 52);
+		assertSame(template, template.scale(1.75));
+		assertEquals(17, 17, 91, 91, template);
 	}
 
 	@SuppressWarnings("static-method")