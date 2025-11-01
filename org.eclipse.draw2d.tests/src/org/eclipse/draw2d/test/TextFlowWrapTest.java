aac3d8101036b7c0cfdfb8bc86636b1243e41838
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-11-01 11:04:24 +0100

    Use assertNull(...) and fail(...) where appropriate
    This replaces calls to assertEquals(null, ...) with assertNull(...) and
    assertTrue(false) with fail().

--- org.eclipse.draw2d.tests/src/org/eclipse/draw2d/test/TextFlowWrapTest.java
+++ org.eclipse.draw2d.tests/src/org/eclipse/draw2d/test/TextFlowWrapTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2023 IBM Corporation and others.
+ * Copyright (c) 2000, 2025 IBM Corporation and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -153,7 +153,7 @@ public class TextFlowWrapTest extends BaseTestCase {
 			// which would cause an index out of bounds exception if there were
 			// no leftover
 			// fragments
-			assertFalse(true, "Failed on: \"" + string1 + "\" + \"" + string2 + "\" Fragment expected: -" + answers[index] //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+			fail("Failed on: \"" + string1 + "\" + \"" + string2 + "\" Fragment expected: -" + answers[index] //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 					+ "- No corresponding fragment\n"); //$NON-NLS-1$
 		}
 	}
