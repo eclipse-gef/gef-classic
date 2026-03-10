9bfafee6a6c03966fef23b4ee25954ed847d3c60
Patrick Ziegler <ziegler.patrick@hotmail.de>
2026-03-10 07:06:34 +0100

    Automatic Code Cleanup: Create array with curly

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/model/Circuit.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/model/Circuit.java
@@ -19,10 +19,10 @@ public class Circuit extends LogicDiagram {
 	static final long serialVersionUID = 1;
 
 	private static int count;
-	public static final String TERMINALS_OUT[] = new String[] { "1", "2", "3", "4", //$NON-NLS-4$//$NON-NLS-3$//$NON-NLS-2$//$NON-NLS-1$
+	public static final String TERMINALS_OUT[] = { "1", "2", "3", "4", //$NON-NLS-4$//$NON-NLS-3$//$NON-NLS-2$//$NON-NLS-1$
 			"5", "6", "7", "8" };//$NON-NLS-4$//$NON-NLS-3$//$NON-NLS-2$//$NON-NLS-1$
 
-	public static final String TERMINALS_IN[] = new String[] { "A", "B", "C", "D", //$NON-NLS-4$//$NON-NLS-3$//$NON-NLS-2$//$NON-NLS-1$
+	public static final String TERMINALS_IN[] = { "A", "B", "C", "D", //$NON-NLS-4$//$NON-NLS-3$//$NON-NLS-2$//$NON-NLS-1$
 			"E", "F", "G", "H" };//$NON-NLS-4$//$NON-NLS-3$//$NON-NLS-2$//$NON-NLS-1$
 
 	@Override
